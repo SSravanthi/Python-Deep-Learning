722b1df3022321780fb1263c4a7c4888256fb45c
Sravanthi Somalaraju <46834796+SSravanthi@users.noreply.github.com>
2019-06-04 12:06:27 -0500

    ICP1

---
+++ ICP1/Source/Reverse.py
@@ -0,0 +1,21 @@
+#Deletes two characters and reverse the output string
+name="Module-1"
+print('welcome to',name)
+
+str = input('Please enter a word: ')
+new=str[2:]
+print(new[::-1])
+
+#'perform arithmetic operations'
+a = int(input('enter a first number'))
+b = int(input('enter a second number'))
+
+print (a,' plus ',b,' equals ',a+b)
+print (a,' multiply ',b,' equals ',a*b)
+print (a,' subtracts ',b,' equals ',a-b)
+print (a,' divides ',b,' equals ',a/b)
+
+# 'replaces the python word to pythons'
+str= input("enter a string: ")
+print(str.replace('python', 'pythons'))
+
